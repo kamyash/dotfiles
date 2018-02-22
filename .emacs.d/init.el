89524da88f8f0d3e0be0a8789ff853a4311d43b1
kamyash <yukikamibayashi@me.com>
2018-02-23 00:53:29 +0900

    [feat] deleted all other than basic settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,65 +1,31 @@
-;; emacs から iTerm を開く
-; (defun execute-on-iterm (command)
-;   (interactive "MCommand: ")
-;   (do-applescript
-;    (format "tell application 'iTerm'
-;               activate
-;               tell current session of current terminal
-;                 write text '%s'
-;               end tell
-;             end tell"
-; 	   command)))
 
-; (defun cd-on-iterm ()
-;   (interactive)
-;   (execute-on-iterm
-;    (format "cd %s" default-directory)))
-   
-;; pythonのコーディング規約チェック
-;(when (load "python-pep8")
-;  (define-key global-map "\C-c\ p" 'python-pep8))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-;; EmacsのGitクライアント
-;(require 'magit)
+(global-linum-mode) ; M-g g -> jump to specific line
 
-;; コンパイルが正常終了した時にウィンドウを自動で閉じる
-;(bury-successful-compilation 1)
-
-;; linum-mode 行番号の表示など
-;; M-g g で特定の行にジャンプ
-(global-linum-mode)
-
-;; ビープ音を消す
 (setq ring-bell-function 'ignore)
 
-;; for NO-GUI environment
-(cond (window-system
-       (set-scroll-bar-mode 'right)))
-
-;; Do not show Emacs startup screen
 (setq inhibit-startup-screen t)
 
-;; 1行ずつスクロール
 (setq scroll-conservatively 35
       scroll-margin 0
-      scroll-step 1)
+      scroll-step 1) ; scroll by 1 line
 
-;; menu bar, tool bar, scroll bar
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode -1)
 
-;; C-hをbackspaceとして使用する
 (keyboard-translate ?\C-h ?\C-?)
 
-;; C-mにnewline-and-indentを割り当てる。初期値はnewline
-(define-key global-map (kbd "C-m") 'newline-and-indent)
+(define-key global-map (kbd "C-m") 'newline-and-indent) ; default: newline
 
-;; "C-t"でウィンドウを切り替える。初期値はtranspose-chars
-(define-key global-map (kbd "C-t") 'other-window)
+(define-key global-map (kbd "C-t") 'other-window) ; default: transpose-chars
 
-;; 言語環境を日本語に(最低限の文字コードの設定がされる)
-(set-language-environment "Japanese")
+(set-language-environment "Japanese") ; least Japanese language environment
 
 (set-terminal-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
@@ -68,207 +34,28 @@
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;; Emacsからの質問をy/nで回答する
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Emacs 23より前のバージョンを利用している場合、
-;; user-emacs-directory変数が未定義のため次の設定を追加
-(when (< emacs-major-version 23)
-  (defvar user-emacs-directory "~/.emacs.d/"))
-
-;; load-path を追加する関数を定義
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-              (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
-
-;; より下に記述したものがPATHの先頭に追加される
-(dolist (dir (list
-	      "/sbin"
-	      "/usr/sbin"
-	      "/bin"
-	      "/usr/bin"
-	      "/opt/local/bin"
-	      "/sw/bin"
-	      "/usr/local/bin"
-	      "/usr/osxws/bin"
-	      "/usr/texbin"
-	      (expand-file-name "~/bin")
-	      (expand-file-name "~/.emacs.d/bin")
-	      ))
-;; PATHとexec-pathに同じものを追加する
-(when (and (file-exists-p dir)(not (member dir exec-path)))
-  (setenv "PATH" (concat dir ":" (getenv "PATH")))
-  (setq exec-path (append (list dir) exec-path))))
-
-;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-;(add-to-load-path "elisp" "conf" "public_repos")
-
-(add-to-list 'load-path "~/.emacs.d/elisp/color-theme")
-;; Color Theme
-;; for trying: M-x load-library RET color-theme RET 
-;;   M-x color-theme-select RET
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-classic)
-;; ウィンドウを半透明に
 (set-frame-parameter nil 'alpha 95)
 
-;; カラム番号も表示
+;; mode line
 (column-number-mode t)
-
-;; ファイルサイズを表示
 (size-indication-mode t)
 
-;; Show full path of the file
-(setq frame-title-format "%f")
+(setq frame-title-format "%f") ; Show full path of the file
 
-;; package.elの設定
-;; パッケージをインストールするには、
-;; M-x list-packages から選択するか、
-;; M-x package-refresh-contents の後に M-x package-initialize パッケージ名
-;; (M-x package-install パッケージ名 の間違い？)
-(package-initialize)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-
-;; clojure-mode
-(use-package clojure-mode)
-
-;; CIDER
-(use-package cider)
-
-;; web-mode の設定
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
-(setq web-mode-engines-alist
-'(("php"    . "\\.phtml\\'")
-  ("blade"  . "\\.blade\\.")))
-
-;; auto-installの設定
-(when (require 'auto-install nil t) 
-  ;; インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  ;; EmacsWikiに登録されているelisp の名前を取得する
-  ;(auto-install-update-emacswiki-package-name t)
-  ;; 必要であればプロキシの設定を行う
-  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
-  ;; install-elisp の関数を利用可能にする
-  (auto-install-compatibility-setup))
-
-;; (auto-install-batch "anything")
-(when (require 'anything nil t)
-  (setq
-   ;; 候補を表示するまでの時間。デフォルトは0.5
-   anything-idle-delay 0.3
-   ;; タイプして再描写するまでの時間。デフォルトは0.1
-   anything-input-idle-delay 0.2
-   ;; 候補の最大表示数。デフォルトは50
-   anything-candidate-number-limit 100
-   ;; 候補が多いときに体感速度を早くする
-   anything-quick-update t
-   ;; 候補選択ショートカットをアルファベットに
-   anything-enable-shortcuts 'alphabet)
-
-  (when (require 'anything-config nil t)
-    ;; root権限でアクションを実行するときのコマンド
-    ;; デフォルトは"su"
-    (setq anything-su-or-sudo "sudo"))
-
-  (require 'anything-match-plugin nil t)
-
-  (when (and (executable-find "cmigemo")
-             (require 'migemo nil t))
-    (require 'anything-migemo nil t))
-
-  (when (require 'anything-complete nil t)
-    ;; lispシンボルの補完候補の再検索時間
-    (anything-lisp-complete-symbol-set-timer 150))
-
-  (require 'anything-show-completion nil t)
-
-  (when (require 'auto-install nil t)
-    (require 'anything-auto-install nil t))
-
-  (when (require 'descbinds-anything nil t)
-    ;; describe-bindingsをAnythingに置き換える
-    (descbinds-anything-install)))
-
-;; Auto Complete Mode
-;; (when (require 'auto-complete-config nil t)
-;;   (add-to-list 'ac-dictionary-directories 
-;;     "~/.emacs.d/elisp/auto-complete-1.3.1/ac-dict")
-;;   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-;;   (ac-config-default))
-;; (global-auto-complete-mode 1)
-
-;; color-moccurの設定
-(when (require 'color-moccur nil t)
-  ;; M-oにoccur-by-moccurを割り当て
-  (define-key global-map (kbd "M-o") 'occur-by-moccur)
-  ;; スペース区切りでAND検索
-  (setq moccur-split-word t)
-  ;; ディレクトリ検索のとき除外するファイル
-  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
-  (add-to-list 'dmoccur-exclusion-mask "^#.+#$")
-  ;; Migemoを利用できる環境であればMigemoを使う
-  (when (and (executable-find "cmigemo")
-             (require 'migemo nil t))
-    (setq moccur-use-migemo t)))
-
-;; undohistの設定
-(when (require 'undohist nil t)
-  (undohist-initialize))
-
-
-;; 対応する括弧をハイライトする。
 (show-paren-mode 1)
 
-;; undo-treeの設定
-(when (require 'undo-tree nil t)
-  (global-undo-tree-mode))
-
-;; point-undoの設定
-(when (require 'point-undo nil t)
-  ;; (define-key global-map [f5] 'point-undo)
-  ;; (define-key global-map [f6] 'point-redo)
-  (define-key global-map (kbd "M-[") 'point-undo)
-  (define-key global-map (kbd "M-]") 'point-redo)
-  )
-
-;;; P97-99 フォントの設定
 (when (eq window-system 'ns)
-  ;; asciiフォントをMenloに
   (set-face-attribute 'default nil
                       :family "Menlo"
                       :height 120)
-  ;; 日本語フォントをヒラギノ明朝 Proに
   (set-fontset-font
    nil 'japanese-jisx0208
-   ;; 英語名の場合
-   ;; (font-spec :family "Hiragino Mincho Pro"))
-;   (font-spec :family "ヒラギノ明朝 Pro"))
    (font-spec :family "Hiragino Kaku Gothic ProN"))
-  ;; ひらがなとカタカナをモトヤシーダに
-  ;; U+3000-303F	CJKの記号および句読点
-  ;; U+3040-309F	ひらがな
-  ;; U+30A0-30FF	カタカナ
   (set-fontset-font
    nil '(#x3040 . #x30ff)
    (font-spec :family "NfMotoyaCedar"))
-  ;; フォントの横幅を調節する
   (setq face-font-rescale-alist
         '((".*Menlo.*" . 1.0)
           (".*Hiragino_Mincho_Pro.*" . 1.2)
@@ -277,45 +64,18 @@
           ("-cdac$" . 1.3))))
 
 (when (eq system-type 'windows-nt)
-  ;; asciiフォントをConsolasに
   (set-face-attribute 'default nil
                       :family "Consolas"
                       :height 120)
-  ;; 日本語フォントをメイリオに
   (set-fontset-font
    nil
    'japanese-jisx0208
    (font-spec :family "メイリオ"))
-  ;; フォントの横幅を調節する
   (setq face-font-rescale-alist
         '((".*Consolas.*" . 1.0)
           (".*メイリオ.*" . 1.15)
           ("-cdac$" . 1.3))))
 
-;; yatex
-(setq auto-mode-alist
-      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-(setq load-path (cons (expand-file-name "~/src/emacs/yatex") load-path))
-(add-to-list 'load-path "~/.emacs.d/elisp/yatex")
-;(setq tex-command "platex")
-(setq tex-command "~/Library/Texshop/bin/platex2pdf-utf8")
-;(setq dvi2-command "xdvi")
-(setq dvi2-command "open -a Preview")
-;(setq bibtex-command "pbibtex")
-(setq dviprint-command-format "dvipdf %s")
-
-;(set-frame-paremeter nil 'alpha 90)
-
-;; Gather backup files and auto-save files
-;; to ~/.emacs.d/backups/
 (add-to-list 'backup-directory-alist
-	     (cons "." "~/.emacs.d/backups/"))
-;(setq auto-save-file-name-transforms
-;      '((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
-
-
-;; dired-mode
-(ffap-bindings)
-(require 'dired-aux)
+	     (cons "." "~/.emacs.d/backups/")) ; gather buckup files and auto-save files
 
